d2733fa49ba8c3c58f6cf7f40759c25d674b1c9f
Sebastiano-Lanfranchi <sebastiano.lanfranchi@gmail.com>
2020-12-17 15:36:57 +0100

    Esercizio finito con console (non stampa a video)

--- js/script.js
+++ js/script.js
@@ -2,18 +2,19 @@
 var i;
 for ( i = 0; i < 100; i++){
   if(i % 15 === 0){
-    i ='FizzBuzz'
-     outputNumeri.innerHTML = '<li>' + i + '</li>';
+     // outputNumeri.innerHTML = '<li>' + 'FizzBuzz' + '</li>';
+     console.log('FizzBuzz')
   }
   else if(i % 3 === 0){
-    i ='Fizz'
-     outputNumeri.innerHTML ='<li>' + i + '</li>';
+     // outputNumeri.innerHTML ='<li>' + 'Fizz' + '</li>';
+     console.log('Fizz')
   }
   else if(i % 5 === 0){
-    i ='Buzz'
-     outputNumeri.innerHTML ='<li>' + i + '</li>';
+     // outputNumeri.innerHTML ='<li>' + 'Buzz' + '</li>';
+     console.log('Buzz')
   }
   else{
-   outputNumeri.innerHTML ='<li>' + i + '</li>';
+   // outputNumeri.innerHTML ='<li>' + i + '</li>';
+   console.log(i)
   }
 }
